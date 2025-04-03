1d7895e9082c8759005990ba13b82a71ccc92e48
Sherman Hunter Sr. <46865433+sdkhunter@users.noreply.github.com>
2025-04-02 18:25:02 -0700

    repaired settings.py
    repaired settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#usyh)_8)vw8984ssoj@#8oseo&%wju3uj+a&8r)*-m%an*!&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
+
 
 
 # Application definition
