9aa07d364418fdba2cdf250df2fb3c28bd483dce
Sherman Hunter Sr. <46865433+sdkhunter@users.noreply.github.com>
2025-04-03 12:04:59 -0700

    project folder changes
    project folder changes

--- cornucopia_strategy/urls.py
+++ cornucopia_strategy/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin-dashboard/', include('features.admin_interface.urls')),
+    path('admin-interface/', include('features.admin_interface.admin_interface.urls')),
     path('', include('features.admin_dashboard.urls')),  # 👈 Root path shows dashboard
     # path('test/', include('features.test.urls')),  # Only include if needed
 ]
